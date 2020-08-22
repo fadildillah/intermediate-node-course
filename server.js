0912cfcec187e213f35e947d20a049f922970065
Muhammad Fadillah <dil@thinkPad>
2020-08-22 15:58:18 +0700

    delete data with DELETE

--- server.js
+++ server.js
@@ -1,72 +1,26 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
 
-const User=require('./models/User');
+const User = require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
-  User.create(
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
+app.post('/users', (req, res) => {
+  User.create({
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
     },
-    (err,data)=>{
-      if (err) {
-        res.json({success: false, message: err})
-      } else if (!data) {
-        res.json({success: false, message: "Not Found"})
-      } else {
-        res.json({success: true, data: data})
-      }
-    })
-})
-
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  User.findById(req.params.id,(err,data)=>{
-    if (err) {
-      res.json({
-        success: false, 
-        message: err
-      })
-    } else if (!data) {
-      res.json({
-        success: false,
-        message: "Not Found"
-      })
-    } else {
-      res.json({
-        success: true,
-        data: data
-      })
-    }
-  })
-})
-// UPDATE
-.put((req,res)=>{
-  User.findByIdAndUpdate(
-    req.params.id,
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
-    {
-      new:true
-    },
-    (err,data)=>{
+    (err, data) => {
       if (err) {
         res.json({
           success: false,
@@ -74,7 +28,7 @@ app.route('/users/:id')
         })
       } else if (!data) {
         res.json({
-          success: false, 
+          success: false,
           message: "Not Found"
         })
       } else {
@@ -83,10 +37,82 @@ app.route('/users/:id')
           data: data
         })
       }
-    }
-  )
+    })
 })
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+
+app.route('/users/:id')
+  // READ
+  .get((req, res) => {
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    })
+  })
+  // UPDATE
+  .put((req, res) => {
+    User.findByIdAndUpdate(
+      req.params.id, {
+        name: req.body.newData.name,
+        email: req.body.newData.email,
+        password: req.body.newData.password
+      }, {
+        new: true
+      },
+      (err, data) => {
+        if (err) {
+          res.json({
+            success: false,
+            message: err
+          })
+        } else if (!data) {
+          res.json({
+            success: false,
+            message: "Not Found"
+          })
+        } else {
+          res.json({
+            success: true,
+            data: data
+          })
+        }
+      }
+    )
+  })
+  // DELETE
+  .delete((req, res) => {
+      User.findByIdAndDelete(
+        req.params.id,
+        (err, data) => {
+          if (err) {
+            res.json({
+              success: false,
+              message: err
+            })
+          } else if (!data) {
+            res.json({
+              success: false,
+              message: "Not Found"
+            })
+          } else {
+            res.json({
+              success: true,
+              data: data
+            })
+          }
+        }
+      )
+    })
